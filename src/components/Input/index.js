cac77f5dec55ac00edfdc5ca4f6398a8b3e526db
Ke Shidong <shidongke213666@sohu-inc.com>
2017-11-01 14:25:22 +0800

    ADD: 添加基础组件和Input组件

---
+++ src/components/Input/index.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import BaseComponent from '../BaseComponent/index';
+import './style.less';
+
+class Input extends BaseComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value,
+        };
+        this.autoBind('handleChange', 'handleKeyPress', 'handleBlur', 'toFocus');
+    }
+
+    toFocus() {
+        this.textInput.focus();
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({
+            value: nextProps.value,
+        });
+    }
+    handleBlur() {
+        if (this.props.onBlur) {
+            this.props.onBlur();
+        }
+    }
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+        if (this.props.onChange) {
+            this.props.onChange({ value: event.target.value, name: this.props.name });
+        }
+    }
+
+    handleKeyPress(event) {
+        if (this.props.onKeyPress) {
+            this.props.onKeyPress(event);
+        }
+    }
+    render() {
+        const {
+            type,
+            disabled,
+            placeholder,
+            error,
+            name,
+            size,
+        } = this.props;
+
+        const clsPrefix = 'c-input';
+        const cls = classNames(clsPrefix, {
+            [`${clsPrefix}__disabled`]: disabled,
+            [`${clsPrefix}__error`]: error,
+            [`${clsPrefix}__${size}`]: true,
+        });
+
+        return (
+            <input
+                className={cls}
+                type={type}
+                name={name}
+                value={this.state.value}
+                placeholder={placeholder}
+                disabled={disabled}
+                onBlur={this.handleBlur}
+                onChange={this.handleChange}
+                onKeyDown={this.handleKeyPress}
+                ref={(input) => { this.textInput = input; }}
+            />
+        );
+    }
+}
+
+Input.defaultProps = {
+    type: 'text',
+    size: 'normal',
+    disabled: false,
+    error: false,
+    name: '',
+    placeholder: '',
+    value: '',
+    onBlur: null,
+    onChange: null,
+    onKeyPress: null,
+};
+
+Input.propTypes = {
+    type: PropTypes.oneOf(['text', 'password']),
+    size: PropTypes.oneOf(['normal', 'large']),
+    disabled: PropTypes.bool,
+    error: PropTypes.bool,
+    name: PropTypes.string,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    onBlur: PropTypes.func,
+    onChange: PropTypes.func,
+    onKeyPress: PropTypes.func,
+};
+
+export default Input;
