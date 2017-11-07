9f22b4ddc8c252bbf2c97c364bc782a71c14225c
Ke Shidong <shidongke213666@sohu-inc.com>
2017-11-07 21:43:38 +0800

    ADD: upload button

--- src/pages/HouseUpload/Coms/UploadButton/index.js
+++ src/pages/HouseUpload/Coms/UploadButton/index.js
@@ -1,23 +1,73 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import BaseComponent from 'components/BaseComponent/index';
 import Button from 'components/Button/index';
 import PropTypes from 'prop-types';
+import { nextStep } from '../../actions';
 import './style.less';
 
-const UploadButton = ({ children }) => {
-    const clsPrefix = 'c-upload-button';
-    return (
-        <div className={clsPrefix}>
-            <Button size="large" className={`${clsPrefix}--button`}>{children}</Button>
-        </div>
-    );
-};
+class UploadButton extends BaseComponent {
+    constructor(props) {
+        super(props);
+        this.autoBind('handlePrev', 'handleNext');
+    }
+    handlePrev() {
+        this.props.onPrev();
+    }
+    handleNext() {
+        if (this.props.curPage === this.props.totalPage) {
+            // 提交
+        } else {
+            // 校验
+            // 更改redux state
+            this.props.dispatch(nextStep(this.props.curPage));
+            // 下一步
+            this.props.onNext();
+        }
+    }
+    render() {
+        const clsPrefix = 'c-upload-button';
+        const {
+            totalPage,
+            curPage,
+        } = this.props;
+        return (
+            <div className={clsPrefix}>
+                {
+                    curPage > 1 ?
+                        <Button
+                            size="large"
+                            className={`${clsPrefix}--button`}
+                            onClick={this.handlePrev}
+                        >
+                            上一步
+                        </Button>
+                        : null
+                }
+                <Button
+                    size="large"
+                    className={`${clsPrefix}--button`}
+                    onClick={this.handleNext}
+                >
+                    {`${curPage === totalPage ? '提交' : '下一步'}`}
+                </Button>
+            </div>
+        );
+    }
+}
 
 UploadButton.propTypes = {
-    children: PropTypes.node,
+    totalPage: PropTypes.number,
+    curPage: PropTypes.number,
+    onPrev: PropTypes.func,
+    onNext: PropTypes.func,
 };
 
 UploadButton.defaultProps = {
-    children: null,
+    totalPage: 1,
+    curPage: 1,
+    onPrev: () => {},
+    onNext: () => {},
 };
 
-export default UploadButton;
+export default connect()(UploadButton);
