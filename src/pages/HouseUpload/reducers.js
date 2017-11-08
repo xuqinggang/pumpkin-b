1501bd96fb1c4d049a6e88d135365d95c5d023dc
Ke Shidong <shidongke213666@sohu-inc.com>
2017-11-08 21:25:35 +0800

    MOD: 添加页面表单校验提示

--- src/pages/HouseUpload/reducers.js
+++ src/pages/HouseUpload/reducers.js
@@ -19,12 +19,20 @@ const commonInfo = (
         },
     }) => (state);
 
+const validateError = (
+    state = {
+        baseInfo: null,
+        roomInfo: null,
+        housePics: null,
+    }) => (state);
+
 export default reduceReducers(
     combineReducers({
         ...RoomInfoReducers,
         ...HousePicsReducers,
         ...BaseInfoReducers,
         commonInfo,
+        validateError,
     }),
     // cross-cutting concerns because here `state` is the whole state tree
     (state, action) => {
@@ -35,38 +43,22 @@ export default reduceReducers(
         case 'house-upload.nextStep':
             switch (action.curPage) {
             case 1: {
-                let newRoomInfo = state.roomInfo;
-                let newHousePics = state.housePics;
-
-                if (state.commonInfo.rentalType !== null) {
-                    // 进入修改数据逻辑
-                    const rentalType = state.baseInfo.rentalType;
-                    const {
-                        room,
-                        saloon,
-                        toilet,
-                    } = state.baseInfo.houseType;
-                    const lastHouseType = state.commonInfo.houseType;
-
-                    if (state.commonInfo.rentalType !== rentalType ||
-                        lastHouseType.room !== room ||
-                        lastHouseType.saloon !== saloon ||
-                        lastHouseType.toilet !== toilet) {
-                        // 当 切换出租类型 或 户型变化 时，初始化之后页面的数据
-                        newRoomInfo = initState('room-info');
-                        newHousePics = initState('house-pics', {
-                            roomNum: room,
-                            saloonNum: saloon,
-                            toiletNum: toilet,
-                        });
-                    }
-                } else {
-                    newRoomInfo = initState('room-info');
+                // 当 切换出租类型 或 户型变化 时，初始化之后页面的数据)
+                const rentalType = state.baseInfo.rentalType;
+                const {
+                    room,
+                    saloon,
+                    toilet,
+                } = state.baseInfo.houseType;
+                const lastHouseType = state.commonInfo.houseType;
+                if (state.commonInfo.rentalType === rentalType &&
+                    lastHouseType.room === room &&
+                    lastHouseType.saloon === saloon &&
+                    lastHouseType.toilet === toilet) {
+                    return state;
                 }
                 return {
                     ...state,
-
-                    // 当
                     // 点击下一步，提取基础信息的公用重要信息，修改可导致其他数据的更改
                     commonInfo: {
                         rentalType: state.baseInfo.rentalType,
@@ -74,13 +66,60 @@ export default reduceReducers(
                             ...state.baseInfo.houseType,
                         },
                     },
-                    roomInfo: newRoomInfo,
+                    roomInfo: initState('room-info'),
+                };
+            }
+            case 2: {
+                const {
+                    room,
+                    saloon,
+                    toilet,
+                } = state.baseInfo.houseType;
+
+                let newHousePics = state.housePics;
+                if (state.commonInfo.rentalType === 0) {
+                    // 整租
+                    newHousePics = initState('house-pics', {
+                        roomNum: room,
+                        saloonNum: saloon,
+                        toiletNum: toilet,
+                    });
+                }
+                if (state.commonInfo.rentalType === 1) {
+                    // 合租
+                    newHousePics = initState('house-pics', {
+                        roomNum: state.roomInfo.length,
+                        saloonNum: saloon,
+                        toiletNum: toilet,
+                    });
+                }
+
+                return {
+                    ...state,
                     housePics: newHousePics,
                 };
             }
             default:
                 return state;
             }
+        case 'house-upload.showValidateError': {
+            return {
+                ...state,
+                validateError: {
+                    ...state.validateError,
+                    [action.pageType]: action.error,
+                },
+            };
+        }
+        case 'house-upload.hideValidateError': {
+            return {
+                ...state,
+                validateError: {
+                    ...state.validateError,
+                    [action.pageType]: null,
+                },
+            };
+        }
         default:
             return state;
         }
