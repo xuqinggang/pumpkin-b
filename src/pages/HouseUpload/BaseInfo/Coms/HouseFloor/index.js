647fa85874e7d5bcb785758728f05f13c84fc3a4
Ke Shidong <shidongke213666@sohu-inc.com>
2017-11-10 18:42:13 +0800

    MOD: 添加错误处理

--- src/pages/HouseUpload/BaseInfo/Coms/HouseFloor/index.js
+++ src/pages/HouseUpload/BaseInfo/Coms/HouseFloor/index.js
@@ -9,14 +9,51 @@ import { setHouseFloor } from '../../actions';
 class HouseFloor extends BaseComponent {
     constructor(props) {
         super(props);
-        this.autoBind('handleChange');
+        this.state = {
+            error: {
+                curFloor: {
+                    error: false,
+                },
+                totalFloor: {
+                    error: false,
+                },
+            },
+        };
+        this.autoBind('handleChange', 'handleBlur');
     }
     handleChange({ name, value }) {
+        this.setState({
+            error: {
+                ...this.state.error,
+                [name]: {
+                    error: false,
+                },
+            },
+        });
+
         this.props.dispatch(setHouseFloor({ name, value }));
+    }
+    handleBlur({ name, value }) {
+        let processValue = value;
+        if (isNaN(Number(value))) {
+            processValue = '';
+        }
+        if (!processValue) {
+            this.setState({
+                error: {
+                    ...this.state.error,
+                    [name]: {
+                        error: true,
+                    },
+                },
+            });
+        }
+        this.props.dispatch(setHouseFloor({ name, value: processValue }));
     }
     render() {
         const clsPrefix = 'c-house-floor';
         const { curFloor, totalFloor } = this.props.houseFloor;
+        const { error } = this.state;
         return (
             <FormItem
                 label="房源楼层"
@@ -27,15 +64,17 @@ class HouseFloor extends BaseComponent {
                     <Input
                         name="curFloor"
                         value={curFloor}
-                        type="number"
                         onChange={this.handleChange}
+                        onBlur={this.handleBlur}
+                        error={error.curFloor.error}
                     />
                     <NoteWord>层／共</NoteWord>
                     <Input
                         name="totalFloor"
                         value={totalFloor}
-                        type="number"
                         onChange={this.handleChange}
+                        onBlur={this.handleBlur}
+                        error={error.totalFloor.error}
                     />
                     <NoteWord>层</NoteWord>
                 </div>
