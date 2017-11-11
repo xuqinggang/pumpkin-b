caa0d8c71593900289dcd3b9b35463f74dc53535
Ke Shidong <shidongke213666@sohu-inc.com>
2017-11-11 20:49:08 +0800

    MOD: 错误校验

--- src/pages/HouseUpload/BaseInfo/Coms/RentalType/index.js
+++ src/pages/HouseUpload/BaseInfo/Coms/RentalType/index.js
@@ -4,14 +4,37 @@ import BaseComponent from 'components/BaseComponent/index';
 import { FormItem } from 'components/Form/index';
 import RadioGroup from 'components/Radio/index';
 import { switchRentalType } from '../../actions';
+import { hideValidateError } from '../../../actions';
 
 class RentalType extends BaseComponent {
     constructor(props) {
         super(props);
+        this.state = {
+            error: {
+                error: false,
+                message: '',
+            },
+        };
         this.autoBind('handleChange');
     }
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.error.error) {
+            this.setState({
+                error: {
+                    ...nextProps.error,
+                },
+            });
+        }
+    }
     handleChange({ value }) {
+        this.setState({
+            error: {
+                ...this.state.error,
+                error: false,
+            },
+        });
         this.props.dispatch(switchRentalType(value));
+        this.props.dispatch(hideValidateError({ pageType: 'baseInfo' }));
     }
     render() {
         const clsPrefix = 'c-rental-type';
@@ -19,6 +42,7 @@ class RentalType extends BaseComponent {
             <FormItem
                 label="出租方式"
                 className={clsPrefix}
+                error={this.state.error}
             >
                 <RadioGroup
                     layout="horizontal"
@@ -41,7 +65,18 @@ class RentalType extends BaseComponent {
 }
 
 export default connect(
-    state => ({
-        value: state.houseUpload.baseInfo.rentalType,
-    }),
+    (state) => {
+        let error = {
+            error: false,
+            message: '',
+        };
+        const baseInfoError = state.houseUpload.validateError.baseInfo;
+        if (baseInfoError && baseInfoError.type === 'rentalType') {
+            error = baseInfoError;
+        }
+        return {
+            error,
+            value: state.houseUpload.baseInfo.rentalType,
+        };
+    },
 )(RentalType);
