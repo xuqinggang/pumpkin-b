f86325a807761ceeebedbfe71757f86bdc633e02
Ke Shidong <shidongke213666@sohu-inc.com>
2017-11-26 18:35:22 +0800

    MOD

--- src/modules/HouseModify/index.js
+++ src/modules/HouseModify/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import BaseComponent from 'components/BaseComponent/index';
 import PageHeader from 'components/PageHeader/index';
 import HouseEdit from 'modules/HouseEdit/index';
+import { decodeQuerySting } from 'utils/index';
 import './style.less';
 
 class HouseModify extends BaseComponent {
     constructor(props) {
         super(props);
-        this.autoBind('handleBack');
+        this.state = {
+            houseId: null,
+        };
+        this.autoBind('handleBack', 'handleTurnPage');
     }
     handleBack() {
-        this.props.onBack();
+        this.props.history.push({
+            pathname: '/house-manage',
+        });
+    }
+    handleTurnPage(houseId) {
+        if (houseId) {
+            this.setState({
+                houseId,
+            });
+        } else {
+            this.props.history.push({
+                pathname: '/house-manage',
+            });
+        }
+    }
+    componentDidMount() {
+        const search = decodeQuerySting(window.location.search);
+        this.handleTurnPage(Number(search.houseId));
     }
     render() {
         const clsPrefix = 'm-house-modify';
@@ -26,20 +48,18 @@ class HouseModify extends BaseComponent {
                     <span className={`${clsPrefix}--title`}>房源编辑 (房源ID: {this.props.houseId})</span>
                 </div>
                 <PageHeader className={`${clsPrefix}--page-title`}>房源编辑</PageHeader>
-                <HouseEdit houseId={this.props.houseId} />
+                <HouseEdit houseId={this.state.houseId} />
             </div>
         );
     }
 }
 
 HouseModify.propTypes = {
-    onBack: PropTypes.func,
     houseId: PropTypes.number,
 };
 
 HouseModify.defaultProps = {
-    onBack: () => {},
     houseId: null,
 };
 
-export default HouseModify;
+export default withRouter(HouseModify);
