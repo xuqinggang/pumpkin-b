db2b29823aeaced6e11c6ca6acc3b29ad981f490
Ke Shidong <shidongke213666@sohu-inc.com>
2017-11-26 20:50:33 +0800

    MOD

--- src/pages/HouseManagePage/index.js
+++ src/pages/HouseManagePage/index.js
@@ -1,45 +1,15 @@
 import React from 'react';
 import MainLayout from 'layouts/MainLayout';
 import BaseComponent from 'components/BaseComponent/index';
-import HouseModify from 'modules/HouseModify/index';
 import HouseManageList from 'modules/HouseManageList/index';
 import './style.less';
 
 class HouseManagePage extends BaseComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            subPageInfo: {
-                type: 'LIST',
-            },
-        };
-        this.autoBind('handleEdit', 'handleBack');
-    }
-    handleEdit({ houseId }) {
-        this.setState({
-            subPageInfo: {
-                type: 'EDIT',
-                houseId,
-            },
-        });
-    }
-    handleBack() {
-        this.setState({
-            subPageInfo: {
-                type: 'LIST',
-            },
-        });
-    }
     render() {
-        const { subPageInfo } = this.state;
         return (
             <MainLayout>
                 <div className="p-house-manage">
-                    {
-                        subPageInfo.type === 'EDIT'
-                            ? <HouseModify onBack={this.handleBack} houseId={subPageInfo.houseId} />
-                            : <HouseManageList onEdit={this.handleEdit} />
-                    }
+                    <HouseManageList />
                 </div>
             </MainLayout>
         );
