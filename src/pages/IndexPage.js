d50c746f2fe92178c32c18f4fa24e105e4bcaa16
keshidong <shidongke213666@sohu-inc.com>
2017-12-04 10:33:30 +0800

    MOD: 首页跳转至房源管理页

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -1,34 +1,13 @@
 // import PropTypes from 'prop-types';
 import { Component } from 'react';
-import { connect } from 'react-redux';
-import fetchTestData from '../actions';
-import MainLayout from '../layouts/MainLayout';
+import { Redirect } from 'react-router';
 
 class IndexPage extends Component {
-    static fetchData(store) {
-        return store.dispatch(fetchTestData());
-    }
-
-    componentDidMount() {
-        const { dispatch } = this.props;
-        if (!window.__INITIAL_STATE__) {
-            dispatch(fetchTestData());
-        }
-    }
-
     render() {
         return (
-            <MainLayout>
-                <div className="">
-                    index
-                </div>
-            </MainLayout>
+            <Redirect to="/house-manage" />
         );
     }
 }
 
-IndexPage.defaultProps = {};
-
-IndexPage.propTypes = {};
-
-export default connect()(IndexPage);
+export default IndexPage;
