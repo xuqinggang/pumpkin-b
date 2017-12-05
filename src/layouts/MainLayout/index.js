55e10a5e3309cecd579b72bf08c86f5e5fed13a2
keshidong <shidongke213666@sohu-inc.com>
2017-12-05 17:07:51 +0800

    MOD: 页面跳转，重置scroll位置

--- src/layouts/MainLayout/index.js
+++ src/layouts/MainLayout/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import BaseComponent from 'components/BaseComponent/index';
 import Header from 'modules/Header/index';
 import Sidebar from 'modules/Sidebar/index';
 import PageTitle from 'modules/PageTitle/index';
 import './style.less';
 
-const MainLayout = ({ title, children }) => (
-    <div className="l-main">
-        <PageTitle title={title} />
-        <Header />
-        <div>
-            <Sidebar />
-            <div className="l-main--content">
-                {children}
+class MainLayout extends BaseComponent {
+    componentDidMount() {
+        window.scroll(0, 0);
+    }
+    render() {
+        return (
+            <div className="l-main">
+                <PageTitle title={this.props.title} />
+                <Header />
+                <div>
+                    <Sidebar />
+                    <div className="l-main--content">
+                        {this.props.children}
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 MainLayout.defaultProps = {
     children: null,
