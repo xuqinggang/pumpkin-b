ac5955668f4aa6ee9602784821b1d46aa82ad83f
keshidong <shidongke213666@sohu-inc.com>
2017-12-05 16:35:30 +0800

    MOD

--- src/modules/HouseEdit/BaseInfo/coms/HouseTypeImage/index.js
+++ src/modules/HouseEdit/BaseInfo/coms/HouseTypeImage/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import axios from 'axios';
 import BaseComponent from 'components/BaseComponent/index';
-import UploadImage from 'components/UploadImage/index';
+import HouseUploadImage from 'modules/HouseUploadImage/index';
 import { setHouseTypeImgUrl } from '../../actions';
-import './style.less';
 
 class HouseTypeImage extends BaseComponent {
     constructor(props) {
         super(props);
         this.autoBind('handleSelect', 'handleDel');
     }
-    handleSelect(dump, file) {
-        const data = new FormData();
-        data.append('file', file);
-        axios.post('/v1/common/pics', data)
-        .then((res) => {
-            const imgUrl = res.data.data.url;
-            this.props.dispatch(setHouseTypeImgUrl(imgUrl));
-        });
+    handleSelect(imgUrl) {
+        this.props.dispatch(setHouseTypeImgUrl(imgUrl));
     }
     handleDel() {
         this.props.dispatch(setHouseTypeImgUrl(''));
@@ -27,14 +19,12 @@ class HouseTypeImage extends BaseComponent {
         const clsPrefix = 'c-house-type-image';
         return (
             <div className={clsPrefix}>
-                <UploadImage
-                    picUrl={this.props.houseTypeImgUrl}
+                <HouseUploadImage
+                    defaultText="上传房源户型图片（选填）"
+                    imgUrl={this.props.houseTypeImgUrl}
                     onSelect={this.handleSelect}
                     onDel={this.handleDel}
-                >
-                    <i className={`${clsPrefix}--indicator icon-add`} />
-                    <div className={`${clsPrefix}--text`}>上传房源户型图片（选填）</div>
-                </UploadImage>
+                />
             </div>
         );
     }
