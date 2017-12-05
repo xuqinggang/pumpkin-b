6e198aa964415db9538dbfac625a44622e841aaf
keshidong <shidongke213666@sohu-inc.com>
2017-12-05 16:35:57 +0800

    MOD

--- src/modules/HouseEdit/BaseInfo/coms/KeeperImage/index.js
+++ src/modules/HouseEdit/BaseInfo/coms/KeeperImage/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import axios from 'axios';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import BaseComponent from 'components/BaseComponent/index';
-import UploadImage from 'components/UploadImage/index';
+import HouseUploadImage from 'modules/HouseUploadImage/index';
 import { errorType } from 'base/types';
 import './style.less';
 
@@ -12,14 +11,8 @@ class KeeperImage extends BaseComponent {
         super(props);
         this.autoBind('handleSelect', 'handleDel');
     }
-    handleSelect(name, file) {
-        const data = new FormData();
-        data.append('file', file);
-        axios.post('/v1/common/pics', data)
-        .then((res) => {
-            const imgUrl = res.data.data.url;
-            this.props.onChange({ name: this.props.name, value: imgUrl });
-        });
+    handleSelect(imgUrl) {
+        this.props.onChange({ name: this.props.name, value: imgUrl });
     }
     handleDel() {
         this.props.onDel();
@@ -31,14 +24,12 @@ class KeeperImage extends BaseComponent {
         });
         return (
             <div className={cls}>
-                <UploadImage
-                    picUrl={this.props.value}
+                <HouseUploadImage
+                    defaultText="上传管家图片（选填）"
+                    imgUrl={this.props.value}
                     onSelect={this.handleSelect}
                     onDel={this.handleDel}
-                >
-                    <i className={`${clsPrefix}--indicator icon-add`} />
-                    <div className={`${clsPrefix}--text`}>上传管家图片（选填）</div>
-                </UploadImage>
+                />
                 <div className={`${clsPrefix}--note`}><span>{this.props.error.message}</span></div>
             </div>
         );
