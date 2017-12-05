256a9146709c356e4546466c4a6ed5d9e025721f
keshidong <shidongke213666@sohu-inc.com>
2017-12-05 16:31:19 +0800

    ADD: 封装上传图片组件

---
+++ src/modules/HouseUploadImage/index.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import BaseComponent from 'components/BaseComponent/index';
+import UploadImage from 'components/UploadImage/index';
+import './style.less';
+
+class HouseUploadImage extends BaseComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: {
+                isError: false,
+                message: '',
+            },
+            loading: {
+                isLoading: false,
+                percent: 0,
+            },
+        };
+        this.autoBind('handleSelect', 'handleDel');
+    }
+    handleSelect(dump, file) {
+        const data = new FormData();
+        data.append('file', file);
+
+        const limitSizeInBytes = 10 * 1024 * 1024;
+        if (file.size >= limitSizeInBytes) {
+            this.setState({
+                error: {
+                    isError: true,
+                    message: '图片大小超过10M',
+                },
+            });
+            return;
+        }
+
+        this.setState({
+            error: {
+                isError: false,
+                message: '',
+            },
+        });
+        axios.post('/v1/common/pics', data, {
+            onUploadProgress: (e) => {
+                this.setState({
+                    loading: {
+                        isLoading: true,
+                        percent: e.loaded / e.total,
+                    },
+                });
+            },
+        })
+            .then((res) => {
+                if (res.data.code === 200) {
+                    const imgUrl = res.data.data.url;
+
+                    // 图片上传成功
+                    this.props.onSelect(imgUrl);
+                } else {
+                    this.setState({
+                        error: {
+                            isError: true,
+                            message: res.data.msg || '图片上传失败',
+                        },
+                    });
+                }
+            })
+            .catch((e) => {
+                this.setState({
+                    error: {
+                        isError: true,
+                        message: e.message,
+                    },
+                });
+            })
+            .then(() => {
+                this.setState({
+                    loading: {
+                        isLoading: false,
+                        percent: 0,
+                    },
+                });
+            });
+    }
+    handleDel() {
+        this.props.onDel();
+    }
+    render() {
+        const clsPrefix = 'c-house-upload-image';
+        return (
+            <div className={clsPrefix}>
+                <UploadImage
+                    imgUrl={this.props.imgUrl}
+                    onSelect={this.handleSelect}
+                    onDel={this.handleDel}
+                    loading={this.state.loading}
+                    error={this.state.error.isError}
+                >
+                    <i className={`${clsPrefix}--indicator icon-add`} />
+                    <div className={`${clsPrefix}--text`}>
+                        {
+                            this.state.error.isError
+                                ? this.state.error.message
+                                : this.props.defaultText
+                        }
+                    </div>
+                </UploadImage>
+            </div>
+        );
+    }
+}
+
+HouseUploadImage.defaultProps = {
+    defaultText: '上传图片',
+    imgUrl: '',
+    onSelect: () => {},
+    onDel: () => {},
+};
+
+HouseUploadImage.propTypes = {
+    defaultText: PropTypes.string,
+    imgUrl: PropTypes.string,
+    onSelect: PropTypes.func,
+    onDel: PropTypes.func,
+};
+
+export default HouseUploadImage;
