634d3ec510ba1f89f78c3389be8b0cb5d9bd7c94
keshidong <shidongke213666@sohu-inc.com>
2017-12-06 21:19:53 +0800

    MOD

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,11 +5,11 @@ export const isNaturalNum = (str) => {
     return false;
 };
 
-export const isFloatNum = (str) => {
-    if (/^[0-9]+([.]{1}[0-9]+){0,1}$/.test(str)) {
-        return Number(str) > 0;
+export const isFloatNum = (str, exact) => {
+    if (exact) {
+        return /^[+-]?\d{0,}[.]{1}\d{1,}$/.test(str);
     }
-    return false;
+    return !isNaN(Number(str)) && Number(str) > 0;
 };
 
 export const isPhoneNo = phone => (/^1[34578]\d{9}$/.test(phone));
