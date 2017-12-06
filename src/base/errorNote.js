869e91bbd17311ed46b1ca50edfef38431d9e016
keshidong <shidongke213666@sohu-inc.com>
2017-12-06 16:01:10 +0800

    MOD

--- src/base/errorNote.js
+++ src/base/errorNote.js
@@ -2,7 +2,7 @@ const errorNote = {
     401: '您暂时未登录，请先登录后进行操作',
     404: '抱歉，您要访问的页面不存在',
     500: '服务器繁忙，请稍后再试',
-    NETWORK_ERR: '请求出现问题，请检查网络连接',
+    NETWORK_ERR: '请检查网络连接',
 };
 
 export default errorNote;
