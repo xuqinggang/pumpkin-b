cfdce052615d2659f0bd35ad51cbf3d050f3f069
keshidong <shidongke213666@sohu-inc.com>
2017-12-11 21:22:21 +0800

    MOD

--- src/pages/HouseModifyPage/index.js
+++ src/pages/HouseModifyPage/index.js
@@ -2,18 +2,73 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import MainLayout from 'layouts/MainLayout';
 import BaseComponent from 'components/BaseComponent/index';
-import HouseModify from 'modules/HouseModify/index';
+import HouseModifyHeader from 'modules/HouseModifyHeader/index';
+import HouseEdit from 'modules/HouseEdit/index';
+import Content from 'components/Content';
+import PublishRentalUnit from 'modules/PublishRentalUnit/index';
+import ShareRentalUnit from 'modules/ShareRentalUnit/index';
+import { decodeQuerySting } from 'utils/index';
 import checkAuthenticated from 'base/checkAuthenticated';
 import './style.less';
 
 class HouseModifyPage extends BaseComponent {
+    constructor(props) {
+        super(props);
+        const search = decodeQuerySting(this.props.history.location.search);
+        this.houseId = Number(search.houseId);
+        this.state = {
+            curPageType: 'EDIT',
+            curTitle: '',
+        };
+        this.pages = {
+            EDIT: {
+                component: HouseEdit,
+                title: '编辑房源',
+                onNext: ({ isAllPublished }) => {
+                    this.handlePageChange(
+                        isAllPublished ? 'SHARE' : 'PUBLISH',
+                        '修改完成',
+                    );
+                },
+            },
+            PUBLISH: {
+                component: PublishRentalUnit,
+                title: '发布房源',
+                onNext: () => {
+                    this.handlePageChange('SHARE', '发布完成');
+                },
+            },
+            SHARE: {
+                component: ShareRentalUnit,
+                title: '分享房源',
+                onNext: () => {},
+            },
+        };
+    }
+    handlePageChange(pageType, title) {
+        this.setState({
+            curPageType: pageType,
+            curTitle: title,
+        });
+    }
     render() {
+        const subPage = this.pages[this.state.curPageType];
         return (
             <MainLayout
                 title="南瓜租房 - 房源编辑"
             >
                 <div className="p-house-modify">
-                    <HouseModify />
+                    <HouseModifyHeader houseId={this.houseId} />
+                    <Content>
+                        {
+                            React.createElement(subPage.component, {
+                                houseId: this.houseId,
+                                title: this.state.curTitle,
+                                subTitle: subPage.title,
+                                onNext: subPage.onNext,
+                            })
+                        }
+                    </Content>
                 </div>
             </MainLayout>
         );
--- src/pages/HouseUploadPage/index.js
+++ src/pages/HouseUploadPage/index.js
@@ -3,19 +3,70 @@ import MainLayout from 'layouts/MainLayout';
 import BaseComponent from 'components/BaseComponent/index';
 import PageHeader from 'components/PageHeader/index';
 import HouseEdit from 'modules/HouseEdit/index';
+import Content from 'components/Content';
+import PublishRentalUnit from 'modules/PublishRentalUnit/index';
+import ShareRentalUnit from 'modules/ShareRentalUnit/index';
 import checkAuthenticated from 'base/checkAuthenticated';
 import './style.less';
 
 class HouseUploadPage extends BaseComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            curPageType: 'EDIT',
+            curTitle: '',
+        };
+        this.houseId = null;
+        this.pages = {
+            EDIT: {
+                component: HouseEdit,
+                title: '编辑房源',
+                onNext: ({ houseId }) => {
+                    // 设置houseId
+                    this.houseId = houseId;
+                    this.handlePageChange('PUBLISH', '上传完成');
+                },
+            },
+            PUBLISH: {
+                component: PublishRentalUnit,
+                title: '发布房源',
+                onNext: () => {
+                    this.handlePageChange('SHARE', '发布完成');
+                },
+            },
+            SHARE: {
+                component: ShareRentalUnit,
+                title: '分享房源',
+                onNext: () => {},
+            },
+        };
+    }
+    handlePageChange(pageType, title) {
+        this.setState({
+            curPageType: pageType,
+            curTitle: title,
+        });
+    }
     render() {
         const clsPrefix = 'p-house-upload';
+        const subPage = this.pages[this.state.curPageType];
+
         return (
             <MainLayout
                 title="南瓜租房 - 房源上传"
             >
                 <div className={clsPrefix}>
                     <PageHeader>房源上传</PageHeader>
-                    <HouseEdit />
+                    <Content>
+                        {
+                            React.createElement(subPage.component, {
+                                houseId: this.houseId,
+                                title: this.state.curTitle,
+                                subTitle: subPage.title,
+                                onNext: subPage.onNext,
+                            })
+                        }
+                    </Content>
                 </div>
             </MainLayout>
         );
